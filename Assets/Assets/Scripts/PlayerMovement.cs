45302c109ded3a47f92922274b0a635dd676b04f
Damian Charkiewicz <charkiewicz.damian@gmail.com>
2018-09-16 20:24:27 +0200

    PlayerMovement/collision

---
+++ Assets/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 6f;
+
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+    
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+    }
+                                        
+    void Move (float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+}
