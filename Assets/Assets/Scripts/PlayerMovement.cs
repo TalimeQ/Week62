3405fb25dd19b37a9597103b7aed9216df2e535f
Damian Charkiewicz <charkiewicz.damian@gmail.com>
2018-09-17 00:04:30 +0200

    Turning + animation controller

--- Assets/Assets/Scripts/PlayerMovement.cs
+++ Assets/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,19 +10,25 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 movement;
     Rigidbody playerRigidbody;
+    Animator animate;
+    float cameraRayLenght = 200f;
+    int board;
     
 
     void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        animate = GetComponent<Animator>();
+        board = LayerMask.GetMask("Board");
     }
 	
 	void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-
         Move(h, v);
+        Turn();
+        Animate(h, v);
     }
                                         
     void Move (float h, float v)
@@ -31,4 +38,31 @@ public class PlayerMovement : MonoBehaviour
         playerRigidbody.MovePosition(transform.position + movement);
     }
 
+    void Turn()
+    {
+        Debug.Log(Input.mousePosition);
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit boardHit;
+        if (Physics.Raycast (cameraRay, out boardHit, cameraRayLenght, board))
+        {
+            Vector3 playerToMouse = boardHit.point - transform.position;
+            playerToMouse.y = 0f;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+
+    }
+
+    void Animate(float h, float v)
+    {
+        bool isWalking = h != 0f || v != 0f;
+
+        if (isWalking)
+            animate.SetTrigger("Run");
+        else
+            animate.SetTrigger("Stop");
+
+    }
+
 }
+
