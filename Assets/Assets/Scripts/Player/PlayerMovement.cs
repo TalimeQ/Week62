b55332c48388eaa28b68bbb7a14707cf56ab7293
Damian Charkiewicz <charkiewicz.damian@gmail.com>
2018-09-18 23:56:10 +0200

    New movement system + city assets

--- Assets/Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Values")]
@@ -11,6 +12,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     [Tooltip("Camera rotation Speed")]
     float cameraSpeed = 10f;
+    [SerializeField]
+    float cameraDistance = 1f;
+    [SerializeField]
+    float lerpParameter = 1f;
 
     Vector3 movement;
     Rigidbody playerRigidbody;
@@ -53,24 +58,39 @@ public class PlayerMovement : MonoBehaviour
 
     void Turn()
     {
-        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit boardHit;
-        if (Physics.Raycast (cameraRay, out boardHit, cameraRayLenght, board))
-        {
-            Vector3 playerToMouse = boardHit.point - transform.position;
-            playerToMouse.y = 0f;
-            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
-            playerRigidbody.MoveRotation(newRotation);
-        }
+        /* Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+         RaycastHit boardHit;
+         if (Physics.Raycast (cameraRay, out boardHit, cameraRayLenght, board))
+         {
+             Vector3 playerToMouse = boardHit.point - transform.position;
+             playerToMouse.y = 0f;
+             Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+             playerRigidbody.MoveRotation(newRotation);
+         }   
+         */
+        Vector3 cameraToPlayer;
+        cameraToPlayer = transform.position - mainCamera.transform.position;
+        cameraToPlayer.y = 0f;
+        Quaternion newRotation = Quaternion.LookRotation(cameraToPlayer);
+        playerRigidbody.MoveRotation(newRotation);
+            
 
     }
     void TurnCamera(float horizontalThrow)
     {
-        float newCameraAngle = mainCamera.transform.localRotation.eulerAngles.y + horizontalThrow * Time.deltaTime * cameraSpeed;
-        float mainCameraEulerX = mainCamera.transform.localRotation.eulerAngles.x;
-        float mainCameraEulerZ = mainCamera.transform.localRotation.eulerAngles.z;
 
-        mainCamera.transform.localRotation = Quaternion.Euler(mainCameraEulerX,newCameraAngle,mainCameraEulerZ);
+        float newX = Mathf.Sin(horizontalThrow * cameraSpeed * Time.deltaTime) * cameraDistance;
+        float newZ = Mathf.Cos(horizontalThrow * cameraSpeed * Time.deltaTime) * cameraDistance;
+        Vector3 newPosition;
+        newPosition.x = newX;
+        newPosition.y = mainCamera.transform.localPosition.y;
+        newPosition.z = newZ;
+        mainCamera.transform.localPosition = Vector3.Lerp(mainCamera.transform.localPosition, newPosition, lerpParameter);
+        //float newCameraAngle = mainCamera.transform.localRotation.eulerAngles.y + horizontalThrow * Time.deltaTime * cameraSpeed;
+        //float mainCameraEulerX = mainCamera.transform.localRotation.eulerAngles.x;
+        //float mainCameraEulerZ = mainCamera.transform.localRotation.eulerAngles.z;
+
+        //mainCamera.transform.localRotation = Quaternion.Euler(mainCameraEulerX,newCameraAngle,mainCameraEulerZ);
 
 
 
